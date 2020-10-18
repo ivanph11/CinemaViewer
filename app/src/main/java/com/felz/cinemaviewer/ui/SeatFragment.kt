dbbd4115236cb31fe15b36cb195c6c73b4609361
Ivanph11 <misterfelz@gmail.com>
2020-10-18 15:29:05 +0800

    Part 2: Seat Map Zoomable (done)

--- app/src/main/java/com/felz/cinemaviewer/ui/SeatFragment.kt
+++ app/src/main/java/com/felz/cinemaviewer/ui/SeatFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
@@ -178,6 +179,7 @@ class SeatFragment : Fragment(),OnItemClickListener {
             seatMapRowAdapter = SeatMapRowAdapter(this@SeatFragment)
             adapter = seatMapRowAdapter
         }
+
     }
     private fun initRecyclerViewDate(){
         recycler_view_date.apply {
